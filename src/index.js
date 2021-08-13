efeb38a49775bf5b369a6fa0a90a08eea3729c5f
Matthew Rodriguez <56925006+mdr20091@users.noreply.github.com>
2021-08-13 10:31:34 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>hello</h1>
+    <p>this is a paragraph</p>
+  </div>,
+
+  document.getElementById("root")
+);
