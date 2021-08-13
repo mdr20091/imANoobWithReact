684dfd977a7f0f8dcea23133abe1911db43f6a6e
Matthew Rodriguez <56925006+mdr20091@users.noreply.github.com>
2021-08-13 12:22:34 -0400

    update update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const imgs = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
-    <h1>hello</h1>
-    <p>this is a paragraph</p>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My favorite Foods
+    </h1>
+    <div>
+      <img alt="random" src={imgs + "?grayscale"} />
+    </div>
   </div>,
-
   document.getElementById("root")
 );
